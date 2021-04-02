eeeb04590c05b4c3841091e3a9cf38678ca5e6d0
juno-choi <ililil9482@naver.com>
2021-04-03 07:44:50 +0900

    jpql 최초 세팅

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,31 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        //db와의 연결
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        //트랜잭션 정의
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        //code
+        try{
+
+            tx.commit();
+        }catch(Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        } finally{
+            //자원 반납
+            em.close();
+        }
+        //자원 반납
+        emf.close();
+    }
+}
